16d13e15264e410e29d81f6ae0b05e0bfbb075d4
Neelam <neelam9673147@gmail.com>
2021-12-20 17:13:43 +0530

    completed

--- js/feedback.js
+++ js/feedback.js
@@ -12,36 +12,90 @@ function checkvalues() {
     alert("Invalid name !");
     return;
   }
+
   var comment = document.getElementById("comment");
-  if (
-    !sal.value.length ||
-    !fname.value.length ||
-    !lname.value.length ||
-    !comment.value.length
-  ) {
-    alert("Fill the form properly.");
+  if (!fname.value.length || !comment.value.length) {
+    alert("First name and comment are mandetory.");
     return;
   }
+
   makeCard(sal, fname, lname, comment);
 }
+let array = [];
 function makeCard(sal, fname, lname, comment) {
+  head = sal.value + " " + fname.value + " " + lname.value + " says...";
+  comment = comment.value;
+
   var n = document.createElement("div");
   n.className = "card";
   var newhead = document.createElement("div");
   var newcontent = document.createElement("div");
   newhead.className = "head";
-  newhead.innerHTML = sal.value + " " + fname.value + " " + lname.value;
+  newhead.innerHTML = head;
   newcontent.className = "content";
-  newcontent.innerHTML = comment.value;
+  newcontent.innerHTML = comment;
   n.appendChild(newhead);
   n.appendChild(newcontent);
   document.querySelector(".heading").after(n);
-  n.classList.add("fade");
-  if (document.getElementsByClassName("card").length == 5) {
-    document.getElementsByClassName("card")[4].remove();
-  }
-  setTimeout(() => n.classList.remove("fade"), 500);
+
+  let obj = {
+    name: head,
+    comment: comment,
+  };
+
+  let array1 = JSON.parse(localStorage.getItem("array"));
+  array.push(obj);
+  console.log(array);
+  localStorage.setItem("array", JSON.stringify(array));
 }
 function hasNumber(myString) {
   return /\d/.test(myString);
 }
+
+function fun() {
+  var sal = document.getElementById("salutation");
+  var fname = document.getElementById("firstName");
+  var lname = document.getElementById("lastName");
+  var comment = document.getElementById("comment");
+
+  sal.value = localStorage.getItem("sal");
+  fname.value = localStorage.getItem("fname");
+  lname.value = localStorage.getItem("lname");
+  comment.value = localStorage.getItem("comment");
+
+  getarray = JSON.parse(localStorage.getItem("array"));
+  console.log(getarray);
+
+  getarray
+    ? getarray.forEach((item) => {
+        var n = document.createElement("div");
+        n.className = "card";
+        var newhead = document.createElement("div");
+        var newcontent = document.createElement("div");
+        newhead.className = "head";
+        newhead.innerHTML = item.name;
+        newcontent.className = "content";
+        newcontent.innerHTML = item.comment;
+        n.appendChild(newhead);
+        n.appendChild(newcontent);
+        document.querySelector(".heading").after(n);
+      })
+    : null;
+}
+
+function reset() {
+  document.getElementById("form1").reset();
+}
+
+var form = document.querySelector("form");
+form.addEventListener("change", function () {
+  var sal = document.getElementById("salutation");
+  var fname = document.getElementById("firstName");
+  var lname = document.getElementById("lastName");
+  var comment = document.getElementById("comment");
+
+  localStorage.setItem("sal", sal.value);
+  localStorage.setItem("fname", fname.value);
+  localStorage.setItem("lname", lname.value);
+  localStorage.setItem("comment", comment.value);
+});
